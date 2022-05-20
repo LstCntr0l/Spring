e467380d092bf2550ad964077643f95c3b8f150b
Krzysztof Buszowski <krzysztof.buszowski@allegro.pl>
2022-05-20 18:02:00 +0200

    Fix issues after submiting form

--- src/main/java/App/schedule/scheduleController.java
+++ src/main/java/App/schedule/scheduleController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Controller
@@ -31,6 +33,59 @@ public class scheduleController {
 
     @GetMapping("/schedule")
     public String harmonogram(Model model) {
+        Schedules value = getAllSchedules();
+        model.addAttribute("schedules", value);
+        return "schedule";
+    }
+
+    @RequestMapping(value = "/schedules", method = RequestMethod.POST)
+    public String insertSchedule(AddScheduleRequestDto request) {
+        Schedule schedule = new Schedule(
+            request.tittle,
+            request.name,
+            request.done
+        );
+        Schedule savedSchedules = scheduleService.Add(schedule);
+        return "redirect:/schedule";
+    }
+
+    @PostMapping("/schedule")
+    public String edit(@ModelAttribute Schedules schedules, BindingResult bindingResult, Model model) {
+        List<Schedules.Schedule.Entry> updatedEntries = schedules.getGroups()
+            .stream()
+            .flatMap(it -> schedules.getGroups().stream())
+            .flatMap(it -> it.getEntries().stream())
+            .toList();
+
+        Map<UUID, Schedule> existingEntires = updatedEntries.stream()
+            .map(Schedules.Schedule.Entry::getId)
+            .map(UUID::fromString)
+            .collect(Collectors.collectingAndThen(
+                Collectors.toList(),
+                scheduleRepository::findAllById
+            ))
+            .stream()
+            .collect(Collectors.toMap(Schedule::getId, Function.identity()));
+
+        List<Schedule> modifiedExistingEntries = updatedEntries.stream()
+            .map(updatedEntry -> {
+                Schedule existingEntry = Optional.ofNullable(existingEntires.get(UUID.fromString(updatedEntry.getId())))
+                    .orElseThrow(() -> new IllegalStateException(String.format("Schedule entry with id %s does not exist!", updatedEntry.getId())));
+
+                existingEntry.setDone(updatedEntry.getDone());
+
+                return existingEntry;
+            })
+            .toList();
+
+        scheduleRepository.saveAll(modifiedExistingEntries);
+
+        model.addAttribute("schedules", getAllSchedules());
+
+        return "schedule";
+    }
+
+    private Schedules getAllSchedules() {
         Map<String, List<Schedule>> groupedByTitle = scheduleService.listAllSchedules()
             .stream()
             .collect(Collectors.groupingBy(Schedule::getTittle));
@@ -61,37 +116,8 @@ public class scheduleController {
             ))
             .collect(Collectors.toCollection(ArrayList::new));
 
-        model.addAttribute("schedules", new Schedules(mappedSchedules));
-        return "schedule";
-    }
-
-
-    @RequestMapping(value = "/schedules", method = RequestMethod.POST)
-    public String insertSchedule(AddScheduleRequestDto request) {
-        Schedule schedule = new Schedule(
-            request.tittle,
-            request.name,
-            request.done
-        );
-        Schedule savedSchedules = scheduleService.Add(schedule);
-        return "redirect:/schedule";
-    }
-
-    @PostMapping("/schedule")
-    public String edit(@ModelAttribute Schedules schedules, BindingResult bindingResult, Model model) {
-        List<Schedules.Schedule.Entry> entries = schedules.getGroups()
-            .stream()
-            .flatMap(it -> schedules.getGroups().stream())
-            .flatMap(it -> it.getEntries().stream())
-            .toList();
-
-        entries.forEach(entry -> {
-            Schedule existingEntry = scheduleRepository.getById(UUID.fromString(entry.getId()));
-            existingEntry.setDone(entry.getDone());
-            scheduleRepository.save(existingEntry);
-        });
-
-        return "schedule";
+        Schedules value = new Schedules(mappedSchedules);
+        return value;
     }
 }
 
